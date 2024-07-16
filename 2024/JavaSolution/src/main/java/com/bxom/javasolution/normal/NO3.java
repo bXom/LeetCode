db5cf0cb6bdc221c64d0352d580d35ee24c143c8
doubleF <lbq0335021@gmail.com>
2024-07-16 22:41:44 +0800

    finish NO3

---
+++ 2024/JavaSolution/src/main/java/com/bxom/javasolution/normal/NO3.java
@@ -0,0 +1,45 @@
+package com.bxom.javasolution.normal;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+public class NO3 {
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() < 1) return 0;
+        int max = 1;
+        int strLen = s.length();
+        int i = 0;
+        int j = 1;
+        while (j <= strLen) {
+            String str = s.substring(i, j);
+            if (checkSub(str)) {
+                if (str.length() > max) {
+                    max = str.length();
+                }
+            } else {
+                i++;
+            }
+            j++;
+        }
+        return max;
+    }
+
+    public static boolean checkSub(String str) {
+        int len = str.length();
+        Set<String> strSet = new HashSet<>();
+        for (int i = 0; i < len; i++) {
+            String s = str.substring(i, i + 1);
+            if (" ".equals(s)) s = "11";
+            if (strSet.contains(s)) return false;
+            strSet.add(s);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        log.info("result: {}", lengthOfLongestSubstring("ss3ds2"));
+    }
+}
