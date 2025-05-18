1881e99e45354e33427aa231095cd6aaa7daa5d4
doubleF <lbq0335021@gmail.com>
2025-05-18 22:36:15 +0800

    init w-1 two sum

---
+++ 75/W1TwoSum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class W1TwoSum {
+    // 试题
+    // https://leetcode.com/problems/two-sum/description/
+    public static void main(String[] args) {
+        solution(new int[]{2, 7, 11, 15}, 9);
+    }
+
+    public static int[] solution(int[] nums, int target) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            int num = nums[i];
+            try {
+                if (numsMap.get(target - num) != null) {
+                    return new int[]{numsMap.get(target - num), i};
+                }
+            } catch (Exception ignored) {
+            } finally {
+                numsMap.put(num, i);
+            }
+        }
+        return new int[]{};
+    }
+}
