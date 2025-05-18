5ab2c5b30dbbd416cc4d5391f33e26e030806979
doubleF <lbq0335021@gmail.com>
2025-05-18 21:49:55 +0800

    add simpleFunc

--- 2024/JavaSolution/src/main/java/com/bxom/javasolution/normal/NO3.java
+++ 2024/JavaSolution/src/main/java/com/bxom/javasolution/normal/NO3.java
@@ -39,7 +39,34 @@ public class NO3 {
         return true;
     }
 
+    public static int simpleFunc(String s) {
+        if (s.length() < 1) return 0;
+        int max = 1;
+        int strLen = s.length();
+        Set<String> strSet = new HashSet<>();
+        int left = 0;
+        int right = 0;
+        for (; left <= right; left++) {
+            String start = s.substring(left, left+1);
+            log.info("start: {}", start);
+            if (!start.equals(s.substring(right, right+1))) strSet.remove(start);
+            for (; right < strLen+1; right++) {
+                log.info("str: {}", s.substring(left, right));
+                String end = s.substring(right, right+1);
+                log.info("end: {}", end);
+                if (strSet.contains(end)) {
+                    break;
+                } else {
+                    strSet.add(end);
+                    if ((right - left) > max) max = right - left;
+                }
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) throws Exception {
-        log.info("result: {}", lengthOfLongestSubstring("ss3ds2"));
+//        log.info("result: {}", lengthOfLongestSubstring("ss3ds2"));
+        log.info("result: {}", simpleFunc("ss3ds2"));
     }
 }
