125349ed8c14c949e8dd44517f00a4356daa1381
doubleF <lbq0335021@gmail.com>
2025-05-21 22:39:24 +0800

    init W0104

---
+++ 75/W0104.java
@@ -0,0 +1,11 @@
+public class W0102 {
+    // 试题
+    // leetcode-121
+    // https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/description/
+    public static void main(String[] args) {
+    }
+
+    public static int solution_1(int[] prices) {
+    }
+
+}
