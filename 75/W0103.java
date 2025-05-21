faad40c21b75c901991cbd562e524893261a5081
doubleF <lbq0335021@gmail.com>
2025-05-21 22:32:49 +0800

    finish W0103

---
+++ 75/W0103.java
@@ -0,0 +1,49 @@
+
+public class W0103 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        System.out.println("result: " + solution_1(l1, l2));
+
+    }
+
+    public static ListNode solution_1(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode(0);
+        ListNode result = temp;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                temp.next = list1;
+                temp = temp.next;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                temp = temp.next;
+                list2 = list2.next;
+            }
+        }
+        if (list1 != null) temp.next = list1;
+        if (list2 != null) temp.next = list2;
+        return result.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
