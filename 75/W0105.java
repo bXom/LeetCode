78ee9d42cdd500c6d519b278925497391c311310
doubleF <lbq0335021@gmail.com>
2025-05-25 16:37:16 +0800

    init W0105

---
+++ 75/W0105.java
@@ -0,0 +1,11 @@
+public class W0105 {
+    // 试题
+    // leetcode-125
+    // https://leetcode.cn/problems/valid-palindrome/description/
+    public static void main(String[] args) {
+        System.out.println("result: " + solution_1(l1, l2));
+    }
+
+    public static boolean solution_1(String s) {
+    }
+}
