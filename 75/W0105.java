843f32bbd3dae56a2e2e353754c41d1143214321
doubleF <lbq0335021@gmail.com>
2025-05-25 17:35:32 +0800

    add solution_1

--- 75/W0105.java
+++ 75/W0105.java
@@ -7,5 +7,14 @@ public class W0105 {
     }
 
     public static boolean solution_1(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
