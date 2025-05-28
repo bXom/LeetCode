74228d9e79548783c38c68c9d0d0c1b6b865b355
doubleF <lbq0335021@gmail.com>
2025-05-28 22:26:00 +0800

    complete W0107

--- 75/W0107.java
+++ 75/W0107.java
@@ -3,11 +3,31 @@ public class W0107 {
     // leetcode-242
     // https://leetcode.cn/problems/valid-anagram/description/
     public static void main(String[] args) {
-        System.out.println("result: " + isAnagram("",""));
+        System.out.println("result: " + isAnagram("", ""));
     }
 
     public static boolean isAnagram(String s, String t) {
-        return false;
+        int len = s.length();
+        if (len != t.length()) return false;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            String sVal = String.valueOf(s.charAt(i));
+            String tVal = String.valueOf(t.charAt(i));
+            if (map.containsKey(sVal)) {
+                map.put(sVal, map.get(sVal) + 1);
+            } else {
+                map.put(sVal, 1);
+            }
+            if (map.containsKey(tVal)) {
+                map.put(tVal, map.get(tVal) - 1);
+            } else {
+                map.put(tVal, -1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) return false;
+        }
+        return true;
     }
 
 }
