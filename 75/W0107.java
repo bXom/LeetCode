efc8bfeb705153c27c4a1ec79ebb772af55b2d30
doubleF <lbq0335021@gmail.com>
2025-05-28 17:42:07 +0800

    init W0107

---
+++ 75/W0107.java
@@ -0,0 +1,13 @@
+public class W0107 {
+    // 试题
+    // leetcode-242
+    // https://leetcode.cn/problems/valid-anagram/description/
+    public static void main(String[] args) {
+        System.out.println("result: " + isAnagram("",""));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        return false;
+    }
+
+}
