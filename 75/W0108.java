b2459218b960f28fe5fbe1248b5a16d70f3a0fef
doubleF <lbq0335021@gmail.com>
2025-05-29 17:04:26 +0800

    finish W0108

--- 75/W0108.java
+++ 75/W0108.java
@@ -3,11 +3,22 @@ public class W0108 {
     // leetcode-704
     // https://leetcode.cn/problems/binary-search/
     public static void main(String[] args) {
-        System.out.println("result: " + search(new int[]{2, 5, 6, 0, 0, 1, 2}, 9));
+        System.out.println("result: " + search(new int[]{9}, 9));
     }
 
     public static int search(int[] nums, int target) {
-
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
     }
-
 }
