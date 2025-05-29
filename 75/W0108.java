222eb14ac8676a9ebedc89bff6e6ca7358ba807c
doubleF <lbq0335021@gmail.com>
2025-05-29 15:34:28 +0800

    init W0108

---
+++ 75/W0108.java
@@ -0,0 +1,13 @@
+public class W0108 {
+    // 试题
+    // leetcode-704
+    // https://leetcode.cn/problems/binary-search/
+    public static void main(String[] args) {
+        System.out.println("result: " + search(new int[]{2, 5, 6, 0, 0, 1, 2}, 9));
+    }
+
+    public static int search(int[] nums, int target) {
+
+    }
+
+}
