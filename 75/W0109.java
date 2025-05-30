53047e12db213f776817400c1f75acaf098873b5
doubleF <lbq0335021@gmail.com>
2025-05-30 17:11:59 +0800

    complete W0108 dfs & bfs

--- 75/W0109.java
+++ 75/W0109.java
@@ -9,15 +9,59 @@ public class W0109 {
     public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int defaultColor = image[sr][sc];
         Set<String> set = new HashSet<>();
-        fill(set, image, sr, sc, defaultColor, color);
+//        dfs(set, image, sr, sc, defaultColor);
+        bfs(set, image, sr, sc, defaultColor);
         for (String s : set) {
+            System.out.println(s);
             String[] ss = s.split(",");
             image[Integer.parseInt(ss[0])][Integer.parseInt(ss[1])] = color;
         }
         return image;
     }
 
-    public static void fill(Set<String> set, int[][] image, int sr, int sc, int defaultColor, int color) {
+    public static void bfs(Set<String> set, int[][] image, int sr, int sc, int defaultColor) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{sr, sc});
+        set.add(sr + "," + sc);
+        while (!queue.isEmpty()) {
+            int[] position = queue.poll();
+            System.out.println("check: position i: " + position[0] + ", j: " + position[1]);
+            if (image.length > position[0] + 1 && image[position[0] + 1][position[1]] == defaultColor) {
+                int i = position[0] + 1;
+                int j = position[1];
+                if (!set.contains(i + "," + j)) {
+                    queue.offer(new int[]{i, j});
+                    set.add(i + "," + j);
+                }
+            }
+            if (image[position[0]].length > (position[1] + 1) && image[position[0]][position[1] + 1] == defaultColor) {
+                int i = position[0];
+                int j = position[1] + 1;
+                if (!set.contains(i + "," + j)) {
+                    queue.offer(new int[]{i, j});
+                    set.add(i + "," + j);
+                }
+            }
+            if (position[0] - 1 >= 0 && image[position[0] - 1][position[1]] == defaultColor) {
+                int i = position[0] - 1;
+                int j = position[1];
+                if (!set.contains(i + "," + j)) {
+                    queue.offer(new int[]{i, j});
+                    set.add(i + "," + j);
+                }
+            }
+            if (position[1] - 1 >= 0 && image[position[0]][position[1] - 1] == defaultColor) {
+                int i = position[0];
+                int j = position[1] - 1;
+                if (!set.contains(i + "," + j)) {
+                    queue.offer(new int[]{i, j});
+                    set.add(i + "," + j);
+                }
+            }
+        }
+    }
+
+    public static void dfs(Set<String> set, int[][] image, int sr, int sc, int defaultColor) {
         if (image[sr][sc] == defaultColor) {
             if (set.contains(sr + "," + sc)) {
                 return;
@@ -28,16 +72,16 @@ public class W0109 {
             return;
         }
         if (image.length > sr + 1 && image[sr + 1][sc] == defaultColor) {
-            fill(set, image, sr + 1, sc, defaultColor, color);
+            dfs(set, image, sr + 1, sc, defaultColor);
         }
         if (image[sr].length > sc + 1 && image[sr][sc + 1] == defaultColor) {
-            fill(set, image, sr, sc + 1, defaultColor, color);
+            dfs(set, image, sr, sc + 1, defaultColor);
         }
         if (sr - 1 >= 0 && image[sr - 1][sc] == defaultColor) {
-            fill(set, image, sr - 1, sc, defaultColor, color);
+            dfs(set, image, sr - 1, sc, defaultColor);
         }
         if (sc - 1 >= 0 && image[sr][sc - 1] == defaultColor) {
-            fill(set, image, sr, sc - 1, defaultColor, color);
+            dfs(set, image, sr, sc - 1, defaultColor);
         }
     }
 }
