058e9d275f052d2fdd7efd55a769d280e9829f41
doubleF <lbq0335021@gmail.com>
2025-06-14 16:11:07 +0800

    init W0113

---
+++ 75/W0113.java
@@ -0,0 +1,32 @@
+public class W0113 {
+    // 试题
+    // leetcode-232
+    // https://leetcode.cn/problems/implement-queue-using-stacks/
+    public static void main(String[] args) {
+        System.out.println("result: " + ("", ""));
+    }
+
+    public static class MyQueue {
+
+    }
+
+    public void MyQueue() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int peek() {
+
+    }
+
+    public boolean empty() {
+
+    }
+}
