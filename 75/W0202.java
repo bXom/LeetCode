391c6a95dc6dd8561cbf20a9cfc92d5fab304121
doubleF <lbq0335021@gmail.com>
2025-06-16 22:24:15 +0800

    init W0202

---
+++ 75/W0202.java
@@ -0,0 +1,12 @@
+public class W0202 {
+    // 试题
+    // leetcode-383
+    // https://leetcode.cn/problems/ransom-note/
+    public static void main(String[] args) {
+        System.out.println("result: " + canConstruct("aa", "ab"));
+    }
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        return true;
+    }
+}
