fd7c3c22966371c952ccfeb04a6f5b16ed114d5c
doubleF <lbq0335021@gmail.com>
2025-06-17 22:28:19 +0800

    add W0202 with func O(m+m)

--- 75/W0202.java
+++ 75/W0202.java
@@ -3,10 +3,31 @@ public class W0202 {
     // leetcode-383
     // https://leetcode.cn/problems/ransom-note/
     public static void main(String[] args) {
-        System.out.println("result: " + canConstruct("aa", "ab"));
+        System.out.println("result: " + o2m("aa", "ab"));
     }
 
-    public static boolean canConstruct(String ransomNote, String magazine) {
+    // 时间复杂度 O(2m)
+    public static boolean o2m(String ransomNote, String magazine) {
+        int ransomSize = ransomNote.length();
+        int magazineSize = magazine.length();
+        if (ransomSize > magazineSize) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < magazineSize; i++) {
+            if (map.containsKey(magazine.charAt(i))) {
+                map.put(magazine.charAt(i), map.get(magazine.charAt(i)) + 1);
+            } else {
+                map.put(magazine.charAt(i), 1);
+            }
+            if (i >= ransomSize) continue;
+            if (map.containsKey(ransomNote.charAt(i))) {
+                map.put(ransomNote.charAt(i), map.get(ransomNote.charAt(i)) - 1);
+            } else {
+                map.put(ransomNote.charAt(i), -1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() < 0) return false;
+        }
         return true;
     }
 }
