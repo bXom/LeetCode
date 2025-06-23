f1009e8d6eb37d007a89ae8a796a4743336e4ab6
doubleF <lbq0335021@gmail.com>
2025-06-23 16:39:55 +0800

    init W0204

---
+++ 75/W0204.java
@@ -0,0 +1,12 @@
+public class W0204 {
+    // 试题
+    // leetcode-409
+    // https://leetcode.cn/problems/longest-palindrome/
+    public static void main(String[] args) {
+        System.out.println("result: " + longestPalindrome("10"));
+    }
+
+    public static int longestPalindrome(String s) {
+        return 0;
+    }
+}
