f1332f2a70431ea7a7650c465672d790481fce6d
doubleF <lbq0335021@gmail.com>
2025-06-23 16:37:49 +0800

    finish W0203

--- 75/W0203.java
+++ 75/W0203.java
@@ -6,7 +6,17 @@ public class W0203 {
         System.out.println("result: " + climbStairs(10));
     }
 
+    // 时间复杂度 O(m+n)
     public static int climbStairs(int n) {
-        return 0;
+        if (n < 3) return n;
+        int a = 1;
+        int b = 2;
+        int result = a + b;
+        for (int i = 3; i < n; i++) {
+            a = b;
+            b = result;
+            result = a + b;
+        }
+        return result;
     }
 }
