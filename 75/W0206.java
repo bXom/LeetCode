f18d491ecf2b016bc750064075861952d8ad1861
doubleF <lbq0335021@gmail.com>
2025-06-23 22:43:50 +0800

    upload solution1 @ W0206

--- 75/W0206.java
+++ 75/W0206.java
@@ -7,6 +7,19 @@ public class W0206 {
     }
 
     public static int majorityElement(int[] nums) {
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            int size = 1;
+            if (map.containsKey(num)) {
+                size += map.get(num);
+            }
+            max = Math.max(max, size);
+            map.put(num, size);
+            if (max > nums.length / 2) {
+                return num;
+            }
+        }
         return 0;
     }
 }
