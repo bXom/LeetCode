d6f8de38769ad4a8f949acff3cd71876f07d3562
doubleF <lbq0335021@gmail.com>
2025-06-23 22:33:54 +0800

    init W0206

---
+++ 75/W0206.java
@@ -0,0 +1,12 @@
+public class W0206 {
+    // 试题
+    // leetcode-169
+    // https://leetcode.cn/problems/majority-element/
+    public static void main(String[] args) {
+        System.out.println("result: " + majorityElement(new int[]{1, 2, 3}));
+    }
+
+    public static int majorityElement(int[] nums) {
+        return 0;
+    }
+}
