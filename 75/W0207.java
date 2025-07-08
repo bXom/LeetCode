79d79f1dd5c1dc6f74f0dd688d62639acbd3b7a7
doubleF <lbq0335021@gmail.com>
2025-07-08 11:22:59 +0800

    complete W0207

--- 75/W0207.java
+++ 75/W0207.java
@@ -3,11 +3,50 @@ public class W0207 {
     // leetcode-67
     // https://leetcode.cn/problems/add-binary/
     public static void main(String[] args) {
-        System.out.println("result: " + ("", ""));
+        System.out.println("result: " + addBinary("", ""));
     }
 
     public static String addBinary(String a, String b) {
-        return "";
+        int aSize = a.length() - 1;
+        int bSize = b.length() - 1;
+        int oneMore = 0;
+        StringBuilder resultBuilder = new StringBuilder();
+        while (aSize >= 0 && bSize >= 0) {
+            int aStr = Integer.parseInt(a.charAt(aSize--) + "");
+            int bStr = Integer.parseInt(b.charAt(bSize--) + "");
+            int sum = aStr + bStr + oneMore;
+            setSum(sum, resultBuilder);
+            oneMore = sum / 2;
+        }
+        while (aSize >= 0) {
+            int aStr = Integer.parseInt(a.charAt(aSize--) + "");
+            int sum = aStr + oneMore;
+            setSum(sum, resultBuilder);
+            oneMore = sum / 2;
+        }
+        while (bSize >= 0) {
+            int bStr = Integer.parseInt(b.charAt(bSize--) + "");
+            int sum = bStr + oneMore;
+            setSum(sum, resultBuilder);
+            oneMore = sum / 2;
+        }
+        if (oneMore == 1) {
+            resultBuilder.append("1");
+        }
+        return resultBuilder.reverse().toString();
     }
 
+    public static void setSum(int sum, StringBuilder sumStr) {
+        switch (sum) {
+            case 2:
+                sumStr.append("0");
+                break;
+            case 3:
+            case 1:
+                sumStr.append("1");
+                break;
+            default:
+                break;
+        }
+    }
 }
