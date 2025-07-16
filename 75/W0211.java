b8352167bd6efe9a7c4748b60c37fab4e2094316
doubleF <lbq0335021@gmail.com>
2025-07-16 22:18:53 +0800

    init W0211

---
+++ 75/W0211.java
@@ -0,0 +1,13 @@
+public class W0211 {
+    // 试题
+    // leetcode-217
+    // https://leetcode.cn/problems/contains-duplicate/
+    public static void main(String[] args) {
+        System.out.println("result: " + containsDuplicate(null));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        return true;
+    }
+
+}
