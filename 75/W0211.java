8758f5ef5039e2a0d3e54bad068f55900a7e72fb
doubleF <lbq0335021@gmail.com>
2025-07-30 22:27:41 +0800

    complete W0211

--- 75/W0211.java
+++ 75/W0211.java
@@ -7,7 +7,22 @@ public class W0211 {
     }
 
     public static boolean containsDuplicate(int[] nums) {
-        return true;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return true;
+            }
+            map.put(num, 1);
+        }
+        return false;
     }
 
+    public static boolean sortSolver(int[] nums) {
+        Arrays.sort(nums);
+        int size = nums.length;
+        for (int i = 0; i < size - 1; i++) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
 }
