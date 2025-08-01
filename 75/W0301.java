29d90bd99e6ac5218292b49ae854679e4738a4bb
doubleF <lbq0335021@gmail.com>
2025-08-01 15:23:46 +0800

    init W0301

---
+++ 75/W0301.java
@@ -0,0 +1,13 @@
+public class W0301 {
+    // 试题
+    // leetcode-57
+    // https://leetcode.cn/problems/insert-interval/
+    public static void main(String[] args) {
+        System.out.println("result: " + insert(new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5}));
+    }
+
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+
+    }
+
+}
